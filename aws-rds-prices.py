248e2365d79627504b415bbf8466fbfde04eaa78
Georgi Tenev <georgi.tenev@tide.com>
2021-01-03 16:51:48 +0200

    take out the price and descrption out of the aws response and print out the db types for which we are searching for

--- aws-rds-prices.py
+++ aws-rds-prices.py
@@ -2,11 +2,19 @@ import requests
 import json
 import sys
 
+DB_TYPE_TO_SEARCH = 'db.t3'
+
 base_url = 'https://pricing.us-east-1.amazonaws.com'
-r = requests.get(base_url+"/offers/v1.0/aws/AmazonRDS/current/region_index.json") 
-json_reponse = r.json()
-json_regions = json_reponse['regions']
-af_south_1_json = json_regions['af-south-1']
+region_index = requests.get(base_url + "/offers/v1.0/aws/AmazonRDS/current/region_index.json").json()
+
+regions = region_index['regions']
+af_south_1_json = regions['af-south-1']
 af_south_1_url = af_south_1_json['currentVersionUrl']
-r = requests.get(base_url+af_south_1_url)
-print(r.text)
+region_index = requests.get(base_url + af_south_1_url).json()
+
+for term_name, term in region_index['terms']['OnDemand'].items():
+    details = list(term.values())[0]
+    price_details = list(details['priceDimensions'].values())[0]
+    description, price = price_details['description'], price_details['pricePerUnit']['USD']
+    if DB_TYPE_TO_SEARCH in description:
+        print(f'{price}, {description}')
